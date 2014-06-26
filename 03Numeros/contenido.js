787e5d69bac5f6f5a9b59dcd2ecfe53645f424cd
sionleojr <sionleojr@gmail.com>
2014-06-26 17:16:41 -0500

    Termino_Cap_4

---
+++ 03Numeros/contenido.js
@@ -0,0 +1,8 @@
+var a;
+a = 5;
+a = 100000;
+a = 123.456;
+a = -500;
+alert(a);
+var frase = "Esta es una frase simple"
+alert(frase.length);
---
+++ 05Comentarios/contenido.js
@@ -0,0 +1,24 @@
+// Esto es un comentario
+var a = 5;
+var b = 10;
+var c = 20;
+
+// Esta es la seccion de booleanas
+a++;
+b--;
+c += 10;
+var x = true;
+var z = false;
+
+/*
+var nombre = prompt("Dime tu \n nombre");
+alert("Hola, "+ nombre);
+*/
+//Esto es un comentario
+/*
+Pero esto ya no lo es
+Pero esto ya no lo es
+Pero esto ya no lo es
+Pero esto ya no lo es
+Pero esto ya no lo es
+*/
